974fba86e9a6a0efb786177491e78ec2937b9750
zhuoshuailiang <162956202+zhuoshuailiang@users.noreply.github.com>
2024-11-01 02:34:25 +0800

    Add files via upload

---
+++ s3_ML_Models/Over10Years/s06_feature_ranking.py
@@ -0,0 +1,110 @@
+
+
+import numpy as np
+import pandas as pd
+import shap
+from tqdm import tqdm
+
+from Utility.Training_Utilities import *
+from Utility.DelongTest import delong_roc_test
+from lightgbm import LGBMClassifier
+from sklearn.metrics import roc_auc_score
+import scipy.stats as stats
+import warnings
+warnings.filterwarnings("ignore")
+pd.options.mode.chained_assignment = None  # default='warn'
+
+# def get_top_pros(mydf):
+#     p_lst = mydf.p_delong.tolist()
+#     i = 0
+#     while((p_lst[i]<0.05)|(p_lst[i+1]<0.05)):
+#         i+=1
+#     return i
+
+dpath = 'C:/Users/Administrator/Desktop/pro/shuai/Data/'
+outpath = 'C:/Users/Administrator/Desktop/pro/shuai/Results/Over10Years/'
+outputfile = outpath + 's6_FeaImpRank.csv'
+
+pro_f_df = pd.read_csv(outpath + 'AccAUC_TotalGain.csv')
+
+nb_top_pros = 2
+
+pro_f_lst = pro_f_df.Pro_code.tolist()[:nb_top_pros]
+pro_df = pd.read_csv(dpath + 'pro_imputed.csv',usecols = ['ID'] + pro_f_lst)
+pro_dict = pd.read_csv(dpath + 'ProCode.csv', usecols = ['Pro_code', 'Prode_finition'])
+target_df = pd.read_csv(dpath + 'outcome.csv')
+cov_f_lst = ['age', 'Pulserate', 'Diabetes', 'units', 'smoke', 'Towns', 'IPAQ', 'BMI', 'hyper', 'viral', 'wctohc']
+
+cov_df = pd.read_csv(dpath + 'nonpro_imputed_int_scaled.csv', usecols = ['ID'] + cov_f_lst)
+reg_df = pd.read_csv(dpath + 'region.csv')
+
+
+mydf = pd.merge(target_df, pro_df, how = 'inner', on = ['ID'])
+mydf = pd.merge(mydf, reg_df, how = 'inner', on = ['ID'])
+mydf = pd.merge(mydf, cov_df, how = 'inner', on = ['ID'])
+fold_id_lst = [i for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+
+
+rm_idx = mydf.index[(mydf.CIRtotal_interuse < 10*365) & (mydf.cirtotal == 1)]
+mydf.drop(rm_idx, axis = 0, inplace = True)
+mydf.reset_index(inplace = True)
+
+my_params = {'n_estimators': 500,
+             'max_depth': 15,
+             'num_leaves': 10,
+             'subsample': 0.7,
+             'learning_rate': 0.01,
+             'colsample_bytree': 0.7}
+
+def normal_imp(mydict):
+    mysum = sum(mydict.values())
+    mykeys = mydict.keys()
+    for key in mykeys:
+        mydict[key] = mydict[key]/mysum
+    return mydict
+
+tmp_f_lst = cov_f_lst + pro_f_lst
+tg_imp_cv = Counter()
+tc_imp_cv = Counter()
+shap_imp_cv = np.zeros(len(tmp_f_lst))
+
+for fold_id in fold_id_lst:
+    train_idx = mydf['Region_code'].index[mydf['Region_code'] != fold_id]
+    test_idx = mydf['Region_code'].index[mydf['Region_code'] == fold_id]
+    X_train, X_test = mydf.iloc[train_idx][tmp_f_lst], mydf.iloc[test_idx][tmp_f_lst]
+    y_train, y_test = mydf.iloc[train_idx].cirtotal, mydf.iloc[test_idx].cirtotal
+    my_lgb = LGBMClassifier(objective = 'binary', metric = 'auc', is_unbalance = True, verbosity = -1, seed = 2024)
+    my_lgb.set_params(**my_params)
+    my_lgb.fit(X_train, y_train)
+    totalgain_imp = my_lgb.booster_.feature_importance(importance_type='gain')
+    totalgain_imp = dict(zip(my_lgb.booster_.feature_name(), totalgain_imp.tolist()))
+    totalcover_imp = my_lgb.booster_.feature_importance(importance_type='split')
+    totalcover_imp = dict(zip(my_lgb.booster_.feature_name(), totalcover_imp.tolist()))
+    tg_imp_cv += Counter(normal_imp(totalgain_imp))
+    tc_imp_cv += Counter(normal_imp(totalcover_imp))
+    explainer = shap.TreeExplainer(my_lgb)
+    shap_values = explainer.shap_values(X_test)
+    shap_values = np.abs(np.average(shap_values[0], axis=0))
+    shap_imp_cv += shap_values / np.sum(shap_values)
+
+
+shap_imp_df = pd.DataFrame({'Pro_code': tmp_f_lst,
+                            'ShapValues_cv': shap_imp_cv/10})
+shap_imp_df.sort_values(by = 'ShapValues_cv', ascending = False, inplace = True)
+
+tg_imp_cv = normal_imp(tg_imp_cv)
+tg_imp_df = pd.DataFrame({'Pro_code': list(tg_imp_cv.keys()),
+                          'TotalGain_cv': list(tg_imp_cv.values())})
+
+tc_imp_cv = normal_imp(tc_imp_cv)
+tc_imp_df = pd.DataFrame({'Pro_code': list(tc_imp_cv.keys()),
+                          'TotalCover_cv': list(tc_imp_cv.values())})
+
+my_imp_df = pd.merge(left = shap_imp_df, right = tg_imp_df, how = 'left', on = ['Pro_code'])
+my_imp_df = pd.merge(left = my_imp_df, right = tc_imp_df, how = 'left', on = ['Pro_code'])
+my_imp_df['Ensemble'] = (my_imp_df['ShapValues_cv'] + my_imp_df['TotalGain_cv'] + my_imp_df['TotalCover_cv'])/3
+my_imp_df.sort_values(by = 'Ensemble', ascending = False, inplace = True)
+
+my_imp_df.to_csv(outputfile, index = False)
+
+print('finished')
---
+++ s3_ML_Models/Over10Years/s99_Combined.py
@@ -0,0 +1,114 @@
+
+
+import numpy as np
+import pandas as pd
+from Utility.Training_Utilities import *
+from Utility.DelongTest import delong_roc_test
+from lightgbm import LGBMClassifier
+from sklearn.metrics import roc_auc_score
+import scipy.stats as stats
+pd.options.mode.chained_assignment = None  # default='warn'
+
+outpath = 'C:/Users/Administrator/Desktop/pro/shuai/Results/Over10Years/'
+
+s1_df = pd.read_csv(outpath + 's1_1Pro.csv')
+s1_df['Category'] = 'SinglePro'
+
+s20_df = pd.read_csv(outpath + 's20_BasicCov.csv')
+s20_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s20_df], axis = 1)
+s20_df['Category'] = 'BasicCovar'
+
+s21_df = pd.read_csv(outpath + 's21_BasicCov.csv')
+s21_df['Category'] = 'SinglePro+BasicCovar'
+
+s22_df = pd.read_csv(outpath + 's22_FibCov.csv')
+s22_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s22_df], axis = 1)
+s22_df['Category'] = 'FibCovar'
+
+s23_df = pd.read_csv(outpath + 's23_FibCov.csv')
+s23_df['Category'] = 'SinglePro+FibCovar'
+
+s24_df = pd.read_csv(outpath + 's24_ApriCov.csv')
+s24_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s24_df], axis = 1)
+s24_df['Category'] = 'ApriCovar'
+
+s25_df = pd.read_csv(outpath + 's25_ApriCov.csv')
+s25_df['Category'] = 'SinglePro+ApriCovar'
+
+s26_df = pd.read_csv(outpath + 's26_Fib_BasicCov.csv')
+s26_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s26_df], axis = 1)
+s26_df['Category'] = 'FibCovar+BasicCovar'
+
+s27_df = pd.read_csv(outpath + 's27_Fib_BasicCov.csv')
+s27_df['Category'] = 'SinglePro+FibCovar+BasicCovar'
+
+s28_df = pd.read_csv(outpath + 's28_Apri_BasicCov.csv')
+s28_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s28_df], axis = 1)
+s28_df['Category'] = 'ApriCovar+BasicCovar'
+
+s29_df = pd.read_csv(outpath + 's29_Apri_BasicCov.csv')
+s29_df['Category'] = 'SinglePro+ApriCovar+BasicCovar'
+
+
+s36_df = pd.read_csv(outpath + 's36_GGTCov.csv')
+s36_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s36_df], axis = 1)
+s36_df['Category'] = 'GGTCovar'
+
+s37_df = pd.read_csv(outpath + 's37_GGTCov.csv')
+s37_df['Category'] = 'SinglePro+GGTCovar'
+
+s38_df = pd.read_csv(outpath + 's38_LabCov.csv')
+s38_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s38_df], axis = 1)
+s38_df['Category'] = 'LabCovar'
+
+s39_df = pd.read_csv(outpath + 's39_LabCov.csv')
+s39_df['Category'] = 'SinglePro+LabCovar'
+
+s40_df = pd.read_csv(outpath + 's40_Lab_BasicCov.csv')
+s40_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s40_df], axis = 1)
+s40_df['Category'] = 'LabCovar+BasicCovar'
+
+s41_df = pd.read_csv(outpath + 's41_Lab_BasicCov.csv')
+s41_df['Category'] = 'SinglePro+LabCovar+BasicCovar'
+
+####
+s30_df = pd.read_csv(outpath + 's30_ProPANEL.csv')
+s30_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s30_df], axis = 1)
+s30_df['Category'] = 'ProPANEL'
+
+s31_df = pd.read_csv(outpath + 's31_ProPANEL_BasicCov.csv')
+s31_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s31_df], axis = 1)
+s31_df['Category'] = 'ProPANEL+BasicCovar'
+
+s32_df = pd.read_csv(outpath + 's32_ProPANEL_FibCov.csv')
+s32_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s32_df], axis = 1)
+s32_df['Category'] = 'ProPANEL+FibCovar'
+
+s33_df = pd.read_csv(outpath + 's33_ProPANEL_ApriCov.csv')
+s33_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s33_df], axis = 1)
+s33_df['Category'] = 'ProPANEL+ApriCovar'
+
+s34_df = pd.read_csv(outpath + 's34_ProPANEL_Fib_BasicCov.csv')
+s34_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s34_df], axis = 1)
+s34_df['Category'] = 'ProPANEL+FibCovar+BasicCovar'
+
+s35_df = pd.read_csv(outpath + 's35_ProPANEL_Apri_BasicCov.csv')
+s35_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s35_df], axis = 1)
+s35_df['Category'] = 'ProPANEL+ApriCovar+BasicCovar'
+
+s42_df = pd.read_csv(outpath + 's42_ProPANEL_LabCov.csv')
+s42_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s42_df], axis = 1)
+s42_df['Category'] = 'ProPANEL+LabCovar'
+
+s43_df = pd.read_csv(outpath + 's43_ProPANEL_Lab_BasicCov.csv')
+s43_df = pd.concat([pd.DataFrame({'Pro_code':[np.nan]}), s43_df], axis = 1)
+s43_df['Category'] = 'ProPANEL+LabCovar+BasicCovar'
+
+
+
+mydf = pd.concat([s1_df, s20_df, s21_df, s22_df, s23_df, s24_df, s25_df, s26_df, s27_df, s28_df, s29_df,
+                  s36_df, s37_df, s38_df, s39_df, s40_df, s41_df,
+                  s30_df, s31_df, s32_df, s33_df, s34_df, s35_df, s42_df, s43_df], axis = 0)
+
+mydf.to_csv(outpath + 'CombineResults.csv', index = False)
+
